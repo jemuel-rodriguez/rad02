39d6d3e57f527f16d81e55d1701d7577bc5c1cac
Eljay Yuzon <eljay@primeoutsourcing.com>
2011-11-29 23:43:08 +0800

    Update wp-content/themes/metamorphosis/includes/theme-widgets.php

--- wp-content/themes/metamorphosis/includes/theme-widgets.php
+++ wp-content/themes/metamorphosis/includes/theme-widgets.php
@@ -1,22 +1,24 @@
 <?php
 
-// =============================== Flickr widget ======================================
+/**
+ * Flickr widget
+ */
 function flickrWidget()
 {
-	$settings = get_option("widget_flickrwidget");
+    $settings = get_option("widget_flickrwidget");
 
-	$id = $settings['id'];
-	$number = $settings['number'];
+    $id = $settings['id'];
+    $number = $settings['number'];
 
 ?>
 
 <div id="flickr" class="block">
-	<h3 class="widget_title"><?php _e('Photos on',primethemes); ?> <span>flick<span>r</span></span></h3>
-	<div class="wrap">
-		<div class="fix"></div>
-		<script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=<?php echo $number; ?>&amp;display=latest&amp;size=s&amp;layout=x&amp;source=user&amp;user=<?php echo $id; ?>"></script>        
-		<div class="fix"></div>
-	</div>
+    <h3 class="widget_title"><?php _e('Photos on',primethemes); ?> <span>flick<span>r</span></span></h3>
+    <div class="wrap">
+        <div class="fix"></div>
+        <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=<?php echo $number; ?>&amp;display=latest&amp;size=s&amp;layout=x&amp;source=user&amp;user=<?php echo $id; ?>"></script>        
+        <div class="fix"></div>
+    </div>
 </div>
 
 <?php
@@ -24,23 +26,23 @@ function flickrWidget()
 
 function flickrWidgetAdmin() {
 
-	$settings = get_option("widget_flickrwidget");
+    $settings = get_option("widget_flickrwidget");
 
-	// check if anything's been sent
-	if (isset($_POST['update_flickr'])) {
-		$settings['id'] = strip_tags(stripslashes($_POST['flickr_id']));
-		$settings['number'] = strip_tags(stripslashes($_POST['flickr_number']));
+    // check if anything's been sent
+    if (isset($_POST['update_flickr'])) {
+        $settings['id'] = strip_tags(stripslashes($_POST['flickr_id']));
+        $settings['number'] = strip_tags(stripslashes($_POST['flickr_number']));
 
-		update_option("widget_flickrwidget",$settings);
-	}
+        update_option("widget_flickrwidget",$settings);
+    }
 
-	echo '<p>
-			<label for="flickr_id">Flickr ID (<a href="http://www.idgettr.com">idGettr</a>):
-			<input id="flickr_id" name="flickr_id" type="text" class="widefat" value="'.$settings['id'].'" /></label></p>';
-	echo '<p>
-			<label for="flickr_number">Number of photos:
-			<input id="flickr_number" name="flickr_number" type="text" class="widefat" value="'.$settings['number'].'" /></label></p>';
-	echo '<input type="hidden" id="update_flickr" name="update_flickr" value="1" />';
+    echo '<p>
+            <label for="flickr_id">Flickr ID (<a href="http://www.idgettr.com">idGettr</a>):
+            <input id="flickr_id" name="flickr_id" type="text" class="widefat" value="'.$settings['id'].'" /></label></p>';
+    echo '<p>
+            <label for="flickr_number">Number of photos:
+            <input id="flickr_number" name="flickr_number" type="text" class="widefat" value="'.$settings['number'].'" /></label></p>';
+    echo '<input type="hidden" id="update_flickr" name="update_flickr" value="1" />';
 
 }
 
